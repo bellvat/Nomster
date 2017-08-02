ca6a4875a3ff7c1575a9c6229d518811ea5e1e48
Monica Sirathanarun <msirathanarun@gmail.com>
2017-08-02 06:08:18 +0000

    Restricted users that are not signed in and not the creater of the place to delete it.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 5)
@@ -20,6 +20,7 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
+#Access the edit form
   def edit
     @place = Place.find(params[:id])
 
@@ -27,15 +28,23 @@ class PlacesController < ApplicationController
       return render text: 'Not Allowed', status: :forbidden
     end
   end
-
+#Updated edited data in the database
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+    
     @place.destroy
     redirect_to root_path
   end
